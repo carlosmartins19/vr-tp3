a76a56c2cac2788b90179166cf7c654907a74ebd
carlos <carlosmartins1994abc@gmail.com>
2018-03-22 16:10:53 +0000

    editar o sh

--- CMDMININET.sh
+++ CMDMININET.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 service openvswitch-switch start
 
 /usr/share/openvswitch/scripts/ovs-ctl start
+ovs-vsctl set-manager ptcp:6640
 
 bash
+
+service openvswitch-switch stop
